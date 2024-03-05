98ab3bf8ffedaef6cb9dcf240609bc791b33d741
Carlos Monteiro <j.de.araujo.monteiro@avanade.com>
2024-03-05 17:46:44 -0300

    feat: implementar nova página.

---
+++ app/lancamentos/variaveis/[month]/[year]/page.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@/src/components/shared/Typography";
+
+export default function VariableReleasesPage() {
+  return (
+    <main className="flex justify-center">
+      <div className="w-[75vw] m-8 p-4 bg-gray-100 rounded">
+        <Typography.Title>Lançamentos Variáveis</Typography.Title>
+      </div>
+    </main>
+  )
+}
--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -4,13 +4,14 @@ import { Typography } from "./Typography";
 const links: { label: string; href: string }[] = [
   { href: '/', label: 'Início' },
   { href: '/lancamentos/fixos', label: 'Lançamentos Fixos' },
+  { href: '/lancamentos/variaveis/março/2024', label: 'Lançamentos Variáveis' },
 ];
 
 export function Navbar() {
   return (
     <nav className="p-4 px-20 bg-blue-400 text-white flex justify-between items-center">
       <Link href={'/'}>
-      <Typography.Subtitle>FinPlan Lite</Typography.Subtitle>
+        <Typography.Subtitle>FinPlan Lite</Typography.Subtitle>
       </Link>
 
       <div className="flex gap-4 justify-end">
